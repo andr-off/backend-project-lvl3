f7464997677757f30de37a742b043833578e4c83
Andr-off <and_ron@mail.ru>
2020-02-08 13:03:04 +0500

    add page-loader executable file

---
+++ src/bin/page-loader.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import commander from 'commander';
+import process from 'process';
+import downloadPage from '..';
+
+const program = new commander.Command();
+
+const main = () => {
+  program
+    .version('0.0.1')
+    .description('Download webpage by link')
+    .option('--output [type]', 'path to directory', process.cwd())
+    .arguments('<link>')
+    .action((link) => {
+      downloadPage(link, program.output);
+    });
+
+  program.parse(process.argv);
+};
+
+main();
