b86b96cb48d12bb2ef4b668dafc9ecaef46c594e
Andr-off <and_ron@mail.ru>
2020-02-14 10:11:41 +0500

    improve tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,52 +9,33 @@ nock.disableNetConnect();
 const getFixturePath = (name) => path.join(__dirname, '../__fixtures__', name);
 
 const testPagePath = getFixturePath('testPage.html');
+const expectedPath = getFixturePath('expected.html');
 
 let tempDirPath;
-
-const table = [
-  [
-    'with pathname in url',
-    'https://hexlet.io',
-    '/courses',
-    'hexlet-io-courses.html',
-  ],
-  [
-    'without pathname in url',
-    'https://hexlet.io',
-    '/',
-    'hexlet-io.html',
-  ],
-  [
-    'with pathname and querystring in url',
-    'https://yandex.ru',
-    '/search/?text=node.js',
-    'yandex-ru-search-text-node-js.html',
-  ],
-];
 
 beforeEach(async () => {
   tempDirPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
 
-test.each(table)(
-  '%s',
-  async (testName, downloadedPageHost, downloadedPagePath, downloadedPageName) => {
-    const downloadedPageLink = `${downloadedPageHost}${downloadedPagePath}`;
-    const downloadedPageFilePath = path.join(tempDirPath, downloadedPageName);
-    const testPageContent = await fs.readFile(testPagePath, 'utf-8');
+test('should work', async () => {
+  const downloadedPageHost = 'https://ru.hexlet.io';
+  const downloadedPagePath = '/courses';
+  const downloadedPageName = 'ru-hexlet-io-courses.html';
+  const downloadedPageLink = `${downloadedPageHost}${downloadedPagePath}`;
+  const downloadedPageFilePath = path.join(tempDirPath, downloadedPageName);
+  const testPageContent = await fs.readFile(testPagePath, 'utf-8');
+  const expected = await fs.readFile(expectedPath, 'utf-8');
 
-    nock(downloadedPageHost)
-      .get(downloadedPagePath)
-      .reply(200, testPageContent);
+  nock(downloadedPageHost)
+    .get(downloadedPagePath)
+    .reply(200, testPageContent);
 
-    await downloadPage(downloadedPageLink, tempDirPath);
+  await downloadPage(downloadedPageLink, tempDirPath);
 
-    const downloadedPageContent = await fs.readFile(downloadedPageFilePath, 'utf-8')
-      .catch((err) => {
-        console.log(err);
-      });
+  const downloadedPageContent = await fs.readFile(downloadedPageFilePath, 'utf-8')
+    .catch((err) => {
+      console.log(err);
+    });
 
-    expect(testPageContent).toBe(downloadedPageContent);
-  },
-);
+  expect(downloadedPageContent).toBe(expected);
+});
